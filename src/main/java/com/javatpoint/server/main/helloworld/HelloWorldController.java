9ef84368fe7f0306099735b00588592c49502ed7
OMKAR HAJARE <omkarh2308@gmail.com>
2022-03-05 23:33:48 +0530

    orignal code as it is

--- src/main/java/com/javatpoint/server/main/helloworld/HelloWorldController.java
+++ src/main/java/com/javatpoint/server/main/helloworld/HelloWorldController.java
@@ -35,8 +35,6 @@ public class HelloWorldController {
 //hello-world/path-variable/javatpoint
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-
-
         return new HelloWorldBean(String.format("Hello World, %s", name));    //%s replace the name
     }
 
